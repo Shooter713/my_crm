ea2cecc686fdb99d77945b4518bdededbed70444
Shooter713 <shooter7ua@gmail.com>
2023-03-30 16:53:58 +0300

    add model

---
+++ Model/Model.php
@@ -0,0 +1,30 @@
+<?php
+Class Model
+{
+    protected $host_name;
+    protected $user_name;
+    protected $database;
+    protected $password;
+    protected $db_handle;
+
+    public function __construct()
+    {
+        $this->host_name = 'localhost';
+        $this->user_name = 'root';
+        $this->database  = 'my_crm';
+        $this->password  = '';
+        $this->db_handle = mysqli_connect($this->host_name, $this->user_name, $this->password);
+        mysqli_select_db($this->db_handle, $this->database) or die(mysqli_error());
+    }
+
+    public function sql($query)
+    {
+        $data = [];
+        $result = mysqli_query($this->db_handle, $query);
+        while ($row = mysqli_fetch_assoc($result)){
+            $data[] = $row;
+        }
+        return $data;
+    }
+
+}
